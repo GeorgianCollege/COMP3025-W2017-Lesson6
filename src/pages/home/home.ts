6ed748e9c0dade77c95a6d8b790d41a1d705646c
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-02-15 11:11:07 -0500

    added game details page and delete functionality

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,17 +1,37 @@
 import { Component } from '@angular/core';
-
-import { NavController } from 'ionic-angular';
-
+import { NavController, AlertController } from 'ionic-angular';
 import {AngularFire, FirebaseListObservable} from 'angularfire2';
+
+// get a reference to the details page
+import { DetailsPage } from '../details/details';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  // PROPERTIES
   games: FirebaseListObservable<any>;
-  constructor(public navCtrl: NavController, af:AngularFire) {
+
+  // CONSTRUCTOR
+  constructor(
+    public navCtrl: NavController,
+    public alertCtrl: AlertController,
+    af:AngularFire) {
     this.games = af.database.list('/games');
+  }
+
+  // METHODS
+  addGame(){
+    this.navCtrl.push(DetailsPage);
+  }
+
+  editGame() {
+    this.navCtrl.push(DetailsPage);
+  }
+
+  deleteGame(game) {
+    this.games.remove(game);
   }
 
 }
